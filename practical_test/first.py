ea509703f23d808ebbb73a6dc8188e7e2fd7e823
First-Demo <shivamgohel2102@gmail.com>
2026-01-12 17:49:43 +0530

    Mutable Default Argument

--- practical_test/first.py
+++ practical_test/first.py
@@ -1,38 +1,8 @@
+def add(list = None):
+    if list is None:
+        list = []
+    return list
 
---------------------15
-class Event:
-    def __init__(self):
-        self.handlers = []
-
-    def subscribe(self, func):
-        self.handlers.append(func)
-
-    def trigger(self):
-        for handler in self.handlers:
-            try:
-                handler()
-            except Exception as e:
-                print("Error ignored:", e)
-
-
-# Handlers (functions)
-def handler_one():
-    print("Handler One executed")
-
-def handler_two():
-    print("Handler Two executed")
-    raise ValueError("Something went wrong")
-
-def handler_three():
-    print("Handler Three executed")
-
-
-# Using the event system
-event = Event()
-
-event.subscribe(handler_one)
-event.subscribe(handler_two)
-event.subscribe(handler_three)
-
-event.trigger()
-
+print(add(1))
+print(add(2))
+print(add(3))
